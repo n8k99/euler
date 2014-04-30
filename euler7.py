d65959c717f2e9d17dd68ed460b7eae7af38352f
Nathan Eckenrode <artist@nathaneckenrode.com>
2014-04-29 23:31:58 -0400

    Brute Force
    I’ve have a brute force solution here. Notes made about more elegant
    method Sieve of Eratosthenes that needs studying to implement

--- euler7.py
+++ euler7.py
@@ -1,3 +1,11 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+# What is the 10 001st prime number?
+
+#this is a brute force method, takes quite a while to get the results
+#there are indications that using the Sieve of Eratosthenes will be
+#much faster and require less computation. I need to look into this
+#further to find out how to do this.
 def isprime(n):
 	n*=1.0
 	if n%2++0 and n!=2 or n%3++0 and n!=3:
@@ -20,7 +28,5 @@ while (count<10001):
 		number=number+1
 		
 	
-# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-# What is the 10 001st prime number?
 			
