d7f956cc5afc41e46aaae042065d49913f1d0fb6
Nathan Eckenrode <artist@nathaneckenrode.com>
2014-04-30 21:11:28 -0400

    scratching start

--- euler9.py
+++ euler9.py
@@ -5,3 +5,18 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.'''
+
+def square(x):
+	x=x*x
+	return x
+
+a = 3
+b = 4
+a= square(a)
+b=b*b
+c=a+b
+print c
+print a+b+c
+print a*b*c
+
+
